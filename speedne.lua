134b4e30fc68271746b907660e49103278d8146a
lamhoc <lamhoc08032007@gmail.com>
2024-10-19 19:49:49 +0700

    Add files via upload

---
+++ speedne.lua
@@ -0,0 +1,15 @@
+-- Speed Script for Roblox
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+-- Set the desired speed
+local speedMultiplier = 2 -- Change this value to increase or decrease speed
+humanoid.WalkSpeed = humanoid.WalkSpeed * speedMultiplier
+
+-- Optional: Reset speed when the player respawns
+player.CharacterAdded:Connect(function(newCharacter)
+    humanoid = newCharacter:WaitForChild("Humanoid")
+    humanoid.WalkSpeed = humanoid.WalkSpeed * speedMultiplier
+end)
